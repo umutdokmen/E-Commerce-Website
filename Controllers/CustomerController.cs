5943f06a7359be9d32d7549e9cb069ea88b7a350
UMUT DÖKMEN <75562773+umutdokmen@users.noreply.github.com>
2021-01-30 00:12:56 +0300

    Add files via upload

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ecommerce_Website.Models.Entity;
+namespace Ecommerce_Website.Controllers
+
+
+{
+    public class CustomerController : Controller
+    {
+
+        // GET: Customer
+        productEntities db = new productEntities();
+        
+        public ActionResult Index(string p)
+        {
+            var values = from v in db.Tbl_Product select v;
+            if (!string.IsNullOrEmpty(p))
+            {
+                values = values.Where(m => m.ProductTitle.Contains(p) || m.ProductDescription.Contains(p));
+            }
+            return View(values.ToList());
+            
+        }
+    }
+}
---
+++ Controllers/ManagementController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ecommerce_Website.Models.Entity;
+
+namespace Ecommerce_Website.Controllers
+{
+    public class ManagementController : Controller
+    {
+
+        // GET: Management
+        
+        productEntities db = new productEntities();
+        public ActionResult Index()
+        {
+            var values = db.Tbl_Product.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult Add_New_Product()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add_New_Product(Tbl_Product p1)
+        {
+
+            db.Tbl_Product.Add(p1);
+            db.SaveChanges();
+            ViewBag.Message = "Product added successfully";
+            return View();
+        }
+        public ActionResult DELETE(int id)
+        {
+            var product = db.Tbl_Product.Find(id);
+            db.Tbl_Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult ProductGet(int id)
+        {
+            var product = db.Tbl_Product.Find(id);
+            return View("ProductGet", product);
+        }
+        public ActionResult Update(Tbl_Product p1)
+        {
+            var product = db.Tbl_Product.Find(p1.ProductCode);
+            product.ProductTitle = p1.ProductTitle;
+            product.ProductDescription = p1.ProductDescription;
+            product.Stock = p1.Stock;
+            product.ProductPrice = p1.ProductPrice;
+            product.ProductImage = p1.ProductImage;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
